8bb397cc92860400357d29b33e15c7ec7b3b61f0
Laurent Kislaire <teebeenator@gmail.com>
2014-03-09 10:14:26 +0100

    setup

--- bin/setup.sh
+++ bin/setup.sh
@@ -1,17 +1,31 @@
 #!/bin/bash
 
 
-L[0]="dot/tmux.conf"
-L[1]="$HOME/.tmux.conf"
-
 MY_PATH="`dirname \"$0\"`"
 MY_PATH="`( cd \"$MY_PATH/../files\" && pwd )`"
 
-
 function link {
-    ln -s "$MY_PATH/$1" $2
+    ln -fs $1 $2
 }
 
-link L[0] L[1]
+function add_link {
+    len=${#LINKS[@]}
+    LINKS[$len]="$MY_PATH/$1"
+    LINKS[$((len+1))]=$2
+}
+
+function mk_links {
+    len=${#LINKS[@]}
+    for ((i=0; i < $len ; i++))
+    do
+        j=$((i+1))
+        link ${LINKS[$i]} ${LINKS[$j]}
+        i=$((i+1))
+    done
+}
 
 
+add_link "dot/tmux.conf" "$HOME/.tmux.conf"
+add_link "dot/gitignore" "$HOME/.gitignore"
+
+mk_links
