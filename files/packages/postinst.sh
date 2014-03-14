3642a9cd577ca62038c87d588f48e5529236befc
Laurent Kislaire <teebeenator@gmail.com>
2014-03-15 00:49:19 +0100

    New packages

--- files/packages/postinst.sh
+++ files/packages/postinst.sh
@@ -1,4 +1,4 @@
 #List of packages to be installed
-PKG=(dlocate apt-file binutils flashplugin-nonfree unp smartmontools gitk strace mplayer debootstrap nmap tmux screen )
+PKG="dlocate apt-file binutils flashplugin-nonfree unp smartmontools gitk strace mplayer debootstrap nmap tmux screen mutt"
 
 sudo aptitude -y install $PKG
