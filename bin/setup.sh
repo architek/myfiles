e51f0c90d61ffe340f29cf51332418ba4cc5ff12
Laurent Kislaire <teebeenator@gmail.com>
2014-03-15 00:42:16 +0100

    Rm link first

--- bin/setup.sh
+++ bin/setup.sh
@@ -5,6 +5,7 @@ MY_PATH="`dirname \"$0\"`"
 MY_PATH="`( cd \"$MY_PATH/../files\" && pwd )`"
 
 function link {
+    rm -rf $2
     ln -fs $1 $2
 }
 
@@ -29,5 +30,6 @@ add_link "dot/tmux.conf" "$HOME/.tmux.conf"
 add_link "dot/gitignore" "$HOME/.gitignore"
 add_link "dot/vimrc" "$HOME/.vimrc"
 add_link "dot/mutt" "$HOME/.mutt"
+add_link "bin/lastinst.sh" "$HOME/bin/lastinst.sh"
 
 mk_links
