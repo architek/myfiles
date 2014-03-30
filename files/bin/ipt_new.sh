c4dba80d1f2efca12f0bfd7992292a8e5526bacd
root <root@debian>
2014-03-30 23:40:29 +0200

    ipt new

--- files/bin/ipt_new.sh
+++ files/bin/ipt_new.sh
@@ -1,1 +1,4 @@
-iptables -I INPUT -m conntrack --ctstate NEW ! -s 127.0.0.1 -j LOG --log-prefix 'New connection'
+iptables -A INPUT -s 127.0.0.1/24 -j ACCEPT
+iptables -A INPUT -s 192.168.1.0/24 -j ACCEPT
+iptables -A INPUT -m conntrack --ctstate NEW -j LOG --log-prefix "New Connection: "
+
