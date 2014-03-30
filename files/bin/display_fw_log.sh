0e009a6717aa8a3267afa3b3b404f597164329d9
Laurent Kislaire <teebeenator@gmail.com>
2014-03-31 00:01:52 +0200

    WIthout hostname

--- files/bin/display_fw_log.sh
+++ files/bin/display_fw_log.sh
@@ -1,6 +1,4 @@
 #Pass a /var/log/message and this makes a nice output of iptables logs for tcp/udp
 
 zgrep "kernel:.*IN=" ${1} | grep -v "224.0.0" | \grep -v "PROTO=ICMP" | sed \
-'s/^\([A-Z][a-z]*[ :0-9]*\).*SRC=\([0-9.]*\) DST=\([0-9.]*\).*TTL=\([0-9]*\).*ID=\([0-9]*\).*PROTO=\([A-Za-z]*\).*SPT=\([0-9]*\)\ DPT=\([0-9]*\).*/\1 \2:\7
-->      \3:\8   TTL=\4  ID=\5       \6  /'
-
+'s/^\([A-Z][a-z]*[ :0-9]*\).*SRC=\([0-9.]*\) DST=\([0-9.]*\).*TTL=\([0-9]*\).*ID=\([0-9]*\).*PROTO=\([A-Za-z]*\).*SPT=\([0-9]*\)\ DPT=\([0-9]*\).*/\1 \2:\7 ->      \3:\8   TTL=\4  ID=\5       \6  /'
