60654860b7b2c7afab1f245b7b60b46efd588735
Laurent Kislaire <teebeenator@gmail.com>
2014-04-02 21:48:33 +0200

    Remove dhcp

--- files/bin/ipt_new.sh
+++ files/bin/ipt_new.sh
@@ -1,4 +1,5 @@
 iptables -A INPUT -s 127.0.0.1/24 -j ACCEPT
 iptables -A INPUT -s 192.168.1.0/24 -j ACCEPT
+iptables -A INPUT -p udp --sport 68 --dport 67 -s 0.0.0.0 -d 255.255.255.255 -j ACCEPT
 iptables -A INPUT -m conntrack --ctstate NEW -j LOG --log-prefix "New Connection: "
 
