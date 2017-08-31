566bd8b5c05951e4a7ec274c95a3d44b20322da2
Laurent Kislaire <teebeenator@gmail.com>
2017-09-01 00:13:32 +0200

    Few more packages

--- files/packages/postinst.sh
+++ files/packages/postinst.sh
@@ -1,4 +1,4 @@
 #List of packages to be installed
-PKG="deborphan dlocate apt-file binutils flashplugin-nonfree unp smartmontools strace mplayer debootstrap nmap tmux screen mutt"
+PKG="deborphan dlocate apt-file binutils unp smartmontools strace mplayer debootstrap nmap tmux screen mutt vim mlocate mencoder"
 
 sudo aptitude -y install $PKG
