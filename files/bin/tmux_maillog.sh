31db206215c00a0ead2819b25c399fe0c979d57c
Laurent Kislaire <teebeenator@gmail.com>
2018-09-12 23:42:23 +0200

    Mmmhh

--- files/bin/tmux_maillog.sh
+++ files/bin/tmux_maillog.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 tmux new-session -n Tmux-MailLog -s mail \
                  'sudo tail -f /var/log/mail.info' \; \
-    split-window 'sudo tail -f /var/log/mail.err' \; \
+    split-window 'sudo tail -f /var/log/mail.log' \; \
     select-layout even-vertical
